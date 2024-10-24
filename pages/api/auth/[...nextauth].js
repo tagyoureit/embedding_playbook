bd8e75074c7f4f06b301e096ea5985ce86f4d47e
Russell Goldin <Russ.Goldin@gmail.com>
2024-10-24 15:44:30 -0700

    set cookies for preview env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,7 +6,7 @@ import { Session } from "models";
 import { UserStore } from "settings";
 
 let cookies = null;
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   cookies = {
     sessionToken: {
       name: `__Secure-next-auth.session-token`,
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
       }
     },
   }
-}
+// }
 
 export const authOptions = {
   cookies,
